74f23709f6de7dbeebf9c32e65d5572518b3f733
MuhammadTariq92 <muhammadtarique822@gmail.com>
2024-05-18 19:17:14 +0500

    Calculator Project Updated-4

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,63 +1,70 @@
 import java.util.Scanner;
+import java.util.NoSuchElementException;
 
 public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("****WELCOME TO CALCULATOR***** ");
 
-        System.out.print("Enter an Expression (e.g., 5 + 10): ");
-        String input = scanner.nextLine();
+        try {
+            System.out.print("Enter an Expression (e.g., 5 + 10): ");
+            String input = scanner.nextLine();
 
-        // Attempt to split input by space
-        String[] parts = input.split("\\s+");
-        double num1, num2;
-        char operator;
+            // Attempt to split input by space
+            String[] parts = input.split("\\s+");
+            double num1, num2;
+            char operator;
 
-        // If splitting by space doesn't work, try splitting by operator
-        if (parts.length != 3) {
-            parts = input.split("[+\\-*/]");
-            if (parts.length != 2) {
-                System.out.println("Invalid input. Please use a valid format, e.g., '5 + 10'");
-                return;
-            }
-            num1 = Double.parseDouble(parts[0]);
-            operator = input.charAt(parts[0].length());
-            num2 = Double.parseDouble(parts[1]);
-        } else {
-            try {
-                num1 = Double.parseDouble(parts[0]);
-                num2 = Double.parseDouble(parts[2]);
-                operator = parts[1].charAt(0);
-            } catch (NumberFormatException | IndexOutOfBoundsException e) {
-                System.out.println("Invalid input. Please use a valid format, e.g., '5 + 10'");
-                return;
-            }
-        }
-
-        double result;
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
-            case '-':
-                result = num1 - num2;
-                break;
-            case '*':
-                result = num1 * num2;
-                break;
-            case '/':
-                if (num2 != 0) {
-                    result = num1 / num2;
-                } else {
-                    System.out.println("Cannot Divide By Zero");
+            // If splitting by space doesn't work, try splitting by operator
+            if (parts.length != 3) {
+                parts = input.split("[+\\-*/]");
+                if (parts.length != 2) {
+                    System.out.println("Invalid input. Please use a valid format, e.g., '5 + 10'");
                     return;
                 }
-                break;
-            default:
-                System.out.println("Invalid operator. Please use one of +, -, *, /");
-                return;
-        }
+                num1 = Double.parseDouble(parts[0]);
+                operator = input.charAt(parts[0].length());
+                num2 = Double.parseDouble(parts[1]);
+            } else {
+                try {
+                    num1 = Double.parseDouble(parts[0]);
+                    num2 = Double.parseDouble(parts[2]);
+                    operator = parts[1].charAt(0);
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    System.out.println("Invalid input. Please use a valid format, e.g., '5 + 10'");
+                    return;
+                }
+            }
 
-        System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+            double result;
+            switch (operator) {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    if (num2 != 0) {
+                        result = num1 / num2;
+                    } else {
+                        System.out.println("Cannot Divide By Zero");
+                        return;
+                    }
+                    break;
+                default:
+                    System.out.println("Invalid operator. Please use one of +, -, *, /");
+                    return;
+            }
+
+            System.out.println("Result: " + num1 + " " + operator + " " + num2 + " = " + result);
+
+        } catch (NoSuchElementException e) {
+            System.out.println("Error: No input provided.");
+            e.printStackTrace();
+        }
     }
 }
